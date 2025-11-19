8f2f1640c14ff427c4d89f270ed6f78d25f3f45b
Heldo Junior <146780453+heldo07@users.noreply.github.com>
2025-11-19 18:03:10 -0300

    Good Solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,7 @@
 package model.entities;
 
+import model.exceptions.DomainException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -12,6 +14,15 @@ public class Reservation {
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException{
+        if(!checkOut.after(checkIn)){
+            throw new DomainException( "check-out date must be after check-in date");
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.roomNumber = roomNumber;
+
+    }
 
 
     public Date getCheckIn() {
@@ -30,29 +41,22 @@ public class Reservation {
         this.roomNumber = roomNumber;
     }
 
-    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
-        this.checkIn = checkIn;
-        this.checkOut = checkOut;
-        this.roomNumber = roomNumber;
-
-    }
 
     public long duration(){
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
 
     }
-    public String updateDates(Date checkIn, Date checkOut){
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException{
         Date now = new Date();
         if(checkIn.before(now) || checkOut.before(now)){
-            return "Reservation dates for update must be future dates: ";
+            throw new DomainException( "Reservation dates for update must be future dates: ");
         }
         if(!checkOut.after(checkIn)){
-            return "check-out date must be after check-in date";
+            throw new DomainException( "check-out date must be after check-in date");
         }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
     }
 
     @Override
